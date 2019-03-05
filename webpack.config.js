2c3adb25a11e9ab51f8f305eba765eb15b595098
Naveen Boopathi <Naveenboopathi18@outlook.com>
2019-03-05 08:29:28 +0530

    Include loader for styles in webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,8 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, use: { loader: 'babel-loader' } }
+      { test: /\.js$/, exclude: /node_modules/, use: { loader: 'babel-loader' } },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] }
     ]
   },
   plugins: [new HtmlWebpackPlugin({
